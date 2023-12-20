c6f7b2cc4deec1fa76d74811474c129fbba19ef2
RamazanIOS <gasratov99@bk.ru>
2023-12-20 09:06:29 +0300

    first commit

---
+++ CombineLessons/CombineLessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineLessonsApp.swift
+//  CombineLessons
+//
+//  Created by Ramazan Gasratov on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CombineLessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
