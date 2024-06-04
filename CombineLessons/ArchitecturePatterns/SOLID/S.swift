efba4bb544ec7150bb34ab187e7d766ffc693558
RamazanIOS <gasratov99@bk.ru>
2024-06-04 22:34:27 +0300

    - SOLID - S

---
+++ CombineLessons/ArchitecturePatterns/SOLID/S.swift
@@ -0,0 +1,14 @@
+//
+//  S.swift
+//  CombineLessons
+//
+//  Created by Ramazan Gasratov on 04.06.2024.
+//
+
+import Foundation
+
+/*
+ Принцип единственной обязанности (Single-Responsibility Principle, SRP): «У клас-
+ са должна быть только одна причина для изменения» (Мартин Р. Принципы, пат-
+ терны и практики гибкой разработки. — 2006).
+ */
